f2144359d7673bb8fd58cb981211ee4ab4bc4d4c
Erik de Jonge <erik@a8.nl>
2015-02-20 11:38:06 +0100

    Update setconfiglinux.sh

--- configscripts/setconfiglinux.sh
+++ configscripts/setconfiglinux.sh
@@ -8,6 +8,6 @@ cat ./config/tokenlinux.txt > ./config/token.txt
 sed -e 's/$num_instances = x/$num_instances = 5/g' Vagrantfile.tpl.rb > Vagrantfile
 sed -i 's/$vm_gui = false/$vm_gui = false/g' Vagrantfile
 sed -i 's/$vm_cpus = x/$vm_cpus = 2/g' Vagrantfile
-sed -i 's/$vm_memory = x/$vm_memory = 2038/g' Vagrantfile
+sed -i 's/$vm_memory = x/$vm_memory = 2048/g' Vagrantfile
 sed -i "s/$update_channel = 'beta'/$update_channel = 'beta'/g" Vagrantfile
 
